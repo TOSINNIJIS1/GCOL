7d6ef50aaef196199bbcd07b355c6eed037a789f
VINCENT ADENIJI <54925113+TOSINNIJIS1@users.noreply.github.com>
2020-10-27 15:38:50 -0400

    width problem fixed

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ function App() {
         [-1, 0]
     ]
 
-  const gridRow = 20;
-  const gridCol = 20;
+  const gridRow = 15;
+  const gridCol = 15;
  
   return (
     <div className="container">
